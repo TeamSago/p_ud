cdfc731eb53c6254dbe276d06497ba152da2d146
jobobdev <tmvle22@gmail.com>
2022-05-10 08:53:07 +0900

    feat: Added homepage with 3 buttons
    for home, login, and logout

--- under10000/homepage/urls.py
+++ under10000/homepage/urls.py
@@ -4,7 +4,7 @@ from django.urls import path
 from . import views
 
 # app urls down here
-
+app_name = "homepage"
 urlpatterns = [
     path("", views.index, name="index"),
 ]
--- under10000/users/urls.py
+++ under10000/users/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import *
+from . import views
 from django.contrib.auth import views as auth_views
 
 app_name = "users"
